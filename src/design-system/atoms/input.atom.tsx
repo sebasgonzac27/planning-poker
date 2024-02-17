db8c7c9fa4a55129ea7c3e4737bfa610e9497fd5
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2024-02-17 10:46:47 -0500

    Refactor input.atom.tsx to handle multiple errors

--- src/design-system/atoms/input.atom.tsx
+++ src/design-system/atoms/input.atom.tsx
@@ -2,20 +2,21 @@ import { ComponentProps } from "react";
 
 interface Props extends ComponentProps<"input"> {
     label: string;
-    error?: string;
+    errors?: string[];
     name: string;
     type: string;
     placeholder: string;
     value: string;
+    required?: boolean;
     onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function InputAtom({ label, error, name, type, placeholder, value, onChange }: Props) {
+export default function InputAtom({ label, errors, name, type, placeholder, value, required, onChange }: Props) {
     return (
         <label htmlFor={name} className="input-atom">
             {label}
-            <input className="input" type={type} name={name} placeholder={placeholder} value={value} onChange={onChange} />
-            {error && <span className="error">{error}</span>}
+            <input className="input" type={type} name={name} placeholder={placeholder} value={value} required={required} onChange={onChange} />
+            {errors && errors.map((error, _index) => <span className="error" key={_index}>{error}</span>)}
         </label>
     )
 }

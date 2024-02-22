850ba2fc6236f664ab7848adab8121e6de8376b6
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2024-02-22 17:37:14 -0500

    Refactor loading component to use PokerAtom and Image component

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,13 @@
-import LoadingMolecule from "@/design-system/molecules/loading.molecule";
+import { PokerAtom } from "@/design-system";
+import Image from "next/image";
 
 export default function Loading() {
     return (
         <div className="loading">
-            <LoadingMolecule />
+            <div className="loader">
+                <PokerAtom spin />
+                <Image src="/images/logo.svg" alt="Logo Pragma" width={149} height={42} />
+            </div>
         </div>
     )
 }

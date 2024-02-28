92b1fb39bf55162e7c7da4dc1bdf82a33f22bb6f
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2024-02-27 23:46:16 -0500

    Remove PokerAtom export

--- src/design-system/index.ts
+++ src/design-system/index.ts
@@ -1,4 +1,3 @@
-export { default as PokerAtom } from './atoms/poker.atom';
 export { default as InputAtom } from './atoms/input.atom';
 export { default as ButtonAtom } from './atoms/button.atom';
 export * from './index';

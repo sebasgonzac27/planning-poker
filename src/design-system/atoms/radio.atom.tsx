84d174187f4a5b4e383eeb2e140eec1a581191d6
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2024-03-02 20:15:54 -0500

    Update radio atom component and add radio styles

--- src/design-system/atoms/radio.atom.tsx
+++ src/design-system/atoms/radio.atom.tsx
@@ -2,15 +2,17 @@ import { ComponentProps } from "react";
 
 interface Props extends ComponentProps<'input'> {
     label: string,
-    isChecked: boolean,
-    name: string
+    isChecked?: boolean,
+    name: string,
+    value: string,
+    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export default function RadioAtom({ label, isChecked, name }: Props) {
+export default function RadioAtom({ label, value, isChecked, name, onChange }: Props) {
     return (
         <label className="radio-atom">
             {label}
-            <input className="input" type="radio" checked={isChecked} name={name} />
+            <input className="input" type="radio" value={value} checked={isChecked} name={name} onChange={onChange} />
         </label>
     )
 }

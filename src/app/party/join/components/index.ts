c2750d1c4930eeb7844e4c01293b21d8b398a0ea
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2024-03-11 00:39:17 -0500

    Refactor join components file structure

---
+++ src/app/party/join/components/index.ts
@@ -0,0 +1,4 @@
+export { default as NewPlayer } from './new-player';
+export { default as Header } from './header/header.component';
+export { default as Desk } from './desk/desk.component'
+export { default as Playground } from './playground'
--- src/app/party/join/components/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as NewPlayer } from './new-player';
-export { default as Header } from './header';
-export { default as Desk } from './desk'
-export { default as Playground } from './playground'

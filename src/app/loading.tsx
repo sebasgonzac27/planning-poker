68d294d23922f92d0e235b45a41bef696794e049
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2024-03-11 00:15:48 -0500

    Add loading module and update loading component

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,12 +1,11 @@
+import styles from './loading.module.scss'
 import Image from "next/image";
 
 export default function Loading() {
     return (
-        <div className="loading">
-            <div className="loader">
-                <Image src="/images/poker.svg" alt="Pocker Pragma" width={60} height={60} className="poker spin" />
-                <Image src="/images/logo.svg" alt="Logo Pragma" width={149} height={42} />
-            </div>
+        <div className={styles.loading}>
+            <Image src="/images/poker.svg" alt="Pocker Pragma" width={60} height={60} className={styles['loading__poker']} />
+            <Image src="/images/logo.svg" alt="Logo Pragma" width={149} height={42} />
         </div>
     )
 }

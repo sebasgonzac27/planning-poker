1215533ba4fc7253c37811e4df6d1ba8e0f81bfc
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2024-03-13 21:06:56 -0500

    Refactor playground component layout and styles

--- src/app/party/join/components/playground/playground.component.tsx
+++ src/app/party/join/components/playground/playground.component.tsx
@@ -1,4 +1,4 @@
-"use client"
+import styles from './playground.module.scss'
 import { Desk } from "..";
 import { usePartyContext } from "../../hooks";
 import Player from '../player/player.component';
@@ -7,28 +7,29 @@ export default function Playground() {
     const { players, socket } = usePartyContext()
     const myPlayer = players.find(p => p.socketId === socket.id);
     const otherPlayers = players.filter(p => p.socketId !== socket.id);
-    const topPlayers = otherPlayers.slice(0, 3);
-    const bottomPlayers = otherPlayers.slice(3, 5);
-    const sidePlayers = otherPlayers.slice(5);
+    const topPlayers = otherPlayers.slice(0, 4);
+    const leftPlayers = otherPlayers.slice(4, 6);
+    const rightPlayers = otherPlayers.slice(6, 8);
+    const bottomPlayers = otherPlayers.slice(8, 11);
 
     return (
-        <section className="join-content">
-            <div className="top-players">
+        <section className={styles.content}>
+            <div className={styles['content__top-players']}>
                 {[...topPlayers].map((p) => <Player key={p.socketId} player={p} />)}
             </div>
-            <div className="middle-area">
-                <div className="side-player">
-                    {sidePlayers[0] && <Player key={sidePlayers[0].socketId} player={sidePlayers[0]} />}
+            <div className={styles['content__middle-area']}>
+                <div className={styles['content__side-players']}>
+                    {[...leftPlayers].map((p) => <Player key={p.socketId} player={p} />)}
                 </div>
                 <Desk />
-                <div className="side-player">
-                    {sidePlayers[1] && <Player key={sidePlayers[1].socketId} player={sidePlayers[1]} />}
+                <div className={styles['content__side-players']}>
+                    {[...rightPlayers].map((p) => <Player key={p.socketId} player={p} />)}
                 </div>
             </div>
-            <div className="top-players">
+            <div className={styles['content__top-players']}>
                 {bottomPlayers[0] && <Player key={bottomPlayers[0].socketId} player={bottomPlayers[0]} />}
                 {myPlayer && <Player key={myPlayer.socketId} player={myPlayer} />}
-                {bottomPlayers[1] && <Player key={bottomPlayers[1].socketId} player={bottomPlayers[1]} />}
+                {[...bottomPlayers].slice(1).map((p) => <Player key={p.socketId} player={p} />)}
             </div>
         </section>
     )

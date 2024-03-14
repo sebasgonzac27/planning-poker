b8ed84d064408494140ee97a94aab5cac04448e2
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2024-03-13 20:15:29 -0500

    Refactor player component styles

--- src/app/party/join/components/player/player.component.tsx
+++ src/app/party/join/components/player/player.component.tsx
@@ -1,19 +1,19 @@
+import styles from './player.module.scss';
 import { Avatar, Card } from "@/components";
 import { Player } from "@/types";
 
 const ROL = {
-    PLAYER: 'player',
-    VIEWER: 'viewer',
+    player: 'player',
+    viewer: 'viewer',
 }
 
 export default function Player({ player }: { player: Player }) {
     return (
-        <div className="player">
-            {player.role === ROL.PLAYER && (
-                <Card>
-                </Card>
+        <div className={styles.player}>
+            {player.role === ROL.player && (
+                <Card variant='small' />
             )}
-            {player.role === ROL.VIEWER && (
+            {player.role === ROL.viewer && (
                 <Avatar variant="large" initials={player.username.slice(0, 2)} />
             )}
             <span>{player.username}</span>

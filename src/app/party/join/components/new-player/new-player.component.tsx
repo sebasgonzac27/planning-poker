67a3757d9a52efe6735ebf6542908a1e56092014
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2024-03-13 20:15:21 -0500

    Add new-player.module.scss and update new-player.component.tsx

--- src/app/party/join/components/new-player/new-player.component.tsx
+++ src/app/party/join/components/new-player/new-player.component.tsx
@@ -1,60 +1,28 @@
+import styles from './new-player.module.scss';
 import { Button, Input, Modal, RadioGroup, Radio } from "@/components"
-import { usePartyContext, useUserContext } from "../../hooks"
-import React, { useEffect, useState } from "react"
-import { Player, PlayerRole } from "@/types"
-import { validateInput } from "@/utils"
+import usePlayerForm from "../../hooks/usePlayerForm"
 
 export default function NewPlayer({ partyId }: { partyId: string }) {
-    const { socket, userLoggedIn, setUserLoggedIn, setPlayers, setPartyName } = usePartyContext()
-    const { setUsername, setRole, setIsOwner } = useUserContext()
-    const [errors, setErrors] = useState<string[]>([])
-
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        const form = e.currentTarget
-        const formData = new FormData(form)
-        const name = formData.get('name') as string
-        const role = formData.get('role-player') as PlayerRole
-
-        socket.emit('join-party', { partyId, name, role })
-        setUserLoggedIn(true)
-    }
-
-    useEffect(() => {
-        socket.on('join-party', ({ party, players }) => {
-            setPartyName(party.name)
-            const me = players.find((p: Player) => p.socketId === socket.id)
-            setPlayers(players)
-            setUsername(me.username)
-            setRole(me.role)
-            setIsOwner(me.isOwner)
-        })
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setErrors(validateInput(e.target.value))
-    }
-
+    const { name, role, errors, userLoggedIn, handleNameChange, handleRoleChange, handleSubmit } = usePlayerForm(partyId)
     return (
         <>
             {!userLoggedIn && (
-                <Modal contentClassName="modal-name">
+                <Modal contentClassName={styles.modal}>
                     <Modal.Body>
-                        <form className="form-name" onSubmit={handleSubmit}>
+                        <form className={styles['modal__form']} onSubmit={handleSubmit}>
                             <Input
                                 label="Tu nombre"
                                 type="text"
                                 placeholder="John Doe"
                                 name="name"
-                                onChange={handleChange}
+                                onChange={handleNameChange}
                                 errors={errors}
                             />
                             <RadioGroup>
-                                <Radio label='Jugador' name="role-player" value={'player'} defaultChecked />
-                                <Radio label='Espectador' name="role-player" value={'viewer'} />
+                                <Radio label='Jugador' name="role-player" value={'player'} defaultChecked onChange={handleRoleChange} />
+                                <Radio label='Espectador' name="role-player" value={'viewer'} onChange={handleRoleChange} />
                             </RadioGroup>
-                            <Button text="Continuar" variant="primary" disabled={errors.length > 0} />
+                            <Button text="Continuar" variant="primary" disabled={errors.length > 0 || !name} />
                         </form>
                     </Modal.Body>
                 </Modal>

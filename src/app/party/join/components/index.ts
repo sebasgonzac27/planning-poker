1095f0104a98c809a988daa9aa4c25bca9b70588
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2024-03-13 20:15:05 -0500

    Add Main component to join components

--- src/app/party/join/components/index.ts
+++ src/app/party/join/components/index.ts
@@ -3,3 +3,4 @@ export { default as Header } from './header/header.component';
 export { default as Desk } from './desk/desk.component';
 export { default as Player } from './player/player.component';
 export { default as Playground } from './playground/playground.component';
+export { default as Main } from './main/main.component';

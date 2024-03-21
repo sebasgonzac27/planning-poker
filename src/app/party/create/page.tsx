ffe907aaf209c84b1a43bdd100309ac6ef90644f
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2024-03-21 16:24:58 -0500

    style: improve bem names

--- src/app/party/create/page.tsx
+++ src/app/party/create/page.tsx
@@ -8,15 +8,15 @@ export default function CreatePartyPage() {
     const { partyName, handleChange, handleSubmit, errors } = useCreateForm();
 
     return (
-        <main className={styles.create}>
-            <header className={styles['create__header']}>
-                <div className={styles['create__banner']}>
+        <main className={styles['create-container']}>
+            <header className={styles['create-container__header']}>
+                <div className={styles['create-container__banner']}>
                     <Image src="/images/poker.svg" alt="Pocker Pragma" width={60} height={60} />
-                    <h1 className={styles['create__title']}>Crear Partida</h1>
+                    <h1 className={styles['create-container__title']}>Crear Partida</h1>
                 </div>
             </header>
-            <section className={styles['create__content']}>
-                <form action="" className={styles['create__form']} onSubmit={handleSubmit}>
+            <section className={styles['create-container__content']}>
+                <form action="" className={styles['create-container__form']} onSubmit={handleSubmit}>
                     <Input label="Nombra la partida" name="name" type="text" placeholder="Sprint 32" value={partyName} required onChange={handleChange} errors={errors} />
                     <Button text="Crear partida" variant="primary" isDisabled={errors.length > 0 || partyName == ''} />
                 </form>

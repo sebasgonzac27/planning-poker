09ec4c94f560f01026cc1805c11ece8e95ef2f99
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2024-03-29 19:52:53 -0500

    fix: export types correctly

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,3 +1,3 @@
-export * from './types/set-state'
+export * from './types/set-state.d'
 export * from './types/player-role'
-export * from './interfaces/player'
+export * from './interfaces/player.d'

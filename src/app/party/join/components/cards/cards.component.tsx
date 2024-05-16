14cba53ec0d3b213cfbac4d05f76f89b224755cb
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2024-05-16 15:53:51 -0500

    feat: add vote functionality to cards component

--- src/app/party/join/components/cards/cards.component.tsx
+++ src/app/party/join/components/cards/cards.component.tsx
@@ -2,15 +2,15 @@ import styles from './cards.module.scss'
 import { Card } from '@/design-system'
 import { usePartyContext, useUserContext } from '../../hooks'
 import { PlayerRole } from '@/core'
+import { vote as Vote } from '@/services/api'
 
 const POINTS = ['1', '2', '3', '5', '8', '13', '21', '34', '55', '89', '?', '☕️']
-const API_URL = process.env.API_URL || 'http://localhost:3001'
 
 export default function Cards () {
   const { role, vote, setVote } = useUserContext()
   const { userLoggedIn, revealed, socket, partyId } = usePartyContext()
 
-  const handleCardClick = (point: string) => {
+  const handleCardClick = async (point: string) => {
     let votePoint: string | null
     if (vote === point) {
       votePoint = null
@@ -19,20 +19,7 @@ export default function Cards () {
     }
 
     setVote(votePoint)
-
-    fetch(`${API_URL}/party/vote`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ vote: votePoint, roomId: partyId, userId: socket.id })
-    }).then((res) => {
-      if (!res.ok) {
-        throw new Error('Error voting')
-      }
-    }).catch((error) => {
-      console.error(error)
-    })
+    await Vote({ vote: votePoint as string, roomId: partyId, userId: socket.id ?? '' })
   }
 
   return (

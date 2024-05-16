0e159e1e742ec62efaec073dcce4320069247251
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2024-05-16 15:55:29 -0500

    refactor: create party from form

--- src/app/party/create/hooks/useCreateForm.ts
+++ src/app/party/create/hooks/useCreateForm.ts
@@ -1,25 +1,25 @@
 import { validateInput } from '@/utils'
 import React, { useState } from 'react'
-import { useCreateParty } from '.'
+import { useRouter } from 'next/navigation'
+import { createParty } from '@/services/api'
 
 export function useCreateForm () {
   const [partyName, setPartyName] = useState<string>('')
   const [errors, setErrors] = useState<string[]>([])
-
-  const { createParty } = useCreateParty()
+  const router = useRouter()
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPartyName(e.target.value)
     setErrors(validateInput(e.target.value))
   }
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     if (errors.length > 0) {
       return
     }
-
-    createParty({ partyName })
+    const id = await createParty(partyName)
+    router.push(`/party/join?partyId=${id}`)
   }
 
   return {

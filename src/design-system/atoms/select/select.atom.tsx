ce2a1da1cfbfb7bd16523427629e4605fc14b9d0
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2024-06-19 12:43:41 -0500

    feat: add Select component to design system

---
+++ src/design-system/atoms/select/select.atom.tsx
@@ -0,0 +1,27 @@
+import { Option } from '@/core'
+import styles from './select.module.scss'
+import { ComponentProps } from 'react'
+
+interface Props extends ComponentProps<'select'> {
+  label: string
+  name: string
+  id: string
+  options: Option[]
+}
+
+export default function Select ({ label, name, id, options }: Props) {
+  return (
+    <div className={styles.select}>
+      <label className={styles.select__label} htmlFor={id}>
+        {label}
+      </label>
+      <select className={styles.select__field} name={name} id={id}>
+        {
+          options.map((option, index) =>
+            <option key={index} value={option.value}>{option.label}</option>
+          )
+        }
+      </select>
+    </div>
+  )
+}

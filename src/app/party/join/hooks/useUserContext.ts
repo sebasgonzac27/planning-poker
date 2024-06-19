586f7b56db408d8a477677581f515a731d9537ff
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2024-06-19 12:33:14 -0500

    refactor: update useUserContext hook to export as default function

--- src/app/party/join/hooks/useUserContext.ts
+++ src/app/party/join/hooks/useUserContext.ts
@@ -1,7 +1,7 @@
 import { useContext } from 'react'
 import { UserContext } from '../contexts'
 
-export function useUserContext () {
+export default function useUserContext () {
   const userContext = useContext(UserContext)
   if (!userContext) {
     throw new Error('useUserContext must be used within a UserProvider')
